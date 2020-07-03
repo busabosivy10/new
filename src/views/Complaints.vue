6a68deedb76d1f5ebf32de9b9771fb4e64e63171
Ivy <busabosivy10@gmail.com>
2020-07-03 22:12:24 +0800

    Third Commit

--- src/views/Complaints.vue
+++ src/views/Complaints.vue
@@ -5,38 +5,38 @@
 			<form @submit.prevent="startSort()">
 				<div class="filter">
 					<h3>Filter</h3>
-					<select v-model="sort.about">
-						<option disabled selected value="0">Month</option>
-						<option value="all">All</option>
-						<option>January</option>
-						<option>February</option>
-						<option>March</option>
-						<option>April</option>
-						<option>May</option>
-						<option>June</option>
-						<option>July</option>
-						<option>August</option>
-						<option>September</option>
-						<option>October</option>
-						<option>November</option>
-						<option>December</option>
-					</select>
-					<select v-model="sort.status">
-						<option disabled selected value="0">Status</option>
-						<option value="all">All</option>
-						<option>Solved</option>
-						<option>Pending</option>
-						<option>Ongoing</option>
-						<option>Canceled</option>
-					</select>
 					<select v-model="sort.way">
-						<option disabled selected value="0">Year</option>
+						<option disabled selected value="0">Cleint Category</option>
 						<option value="all">All</option>
-						<option>2019</option>
-						<option>2020</option>
-						<option>2021</option>
-						<option>2022</option>
+						<option>Student</option>
+						<option>Faculty</option>
+						<option>Industry</option>
+						<option>Supplier</option>
+						<option>Regalutary Body</option>
+						<option>Alumni</option>
+						<option>Non-teaching</option>
+						<option>Community</option>
 					</select>
+
+					<select v-model="sort.target">
+						<option disabled selected value="0">About</option>
+						<option>Facilities</option>
+						<option>Instructor</option>
+						<option>Portal</option>
+						<option>Guidance</option>
+						<option>Staff</option>
+						<option>Event</option>
+						<option>Library</option>
+						<option>Registrar</option>
+					</select>
+
+					<select v-model="sort.about">
+						<option disabled selected value="0">Subject</option>
+						<option value="all">All</option>
+						<option>Suggestion</option>
+						<option>Feedback</option>
+					</select>
+
 					<button type="submit">Go</button>
 				</div>
 			</form>
@@ -60,7 +60,7 @@
 				complaints: [],
 				sort: {
 					way: 0,
-					status: 0,
+					target: 0,
 					about: 0,
 				}
 			}
@@ -73,7 +73,7 @@
 				me.complaints = []
 
 				let w = (me.sort.way == 0 || me.sort.way == 'all') ? '0' : '1'
-				let t = (me.sort.staus == 0 || me.sort.status == 'all') ? '0' : '1'
+				let t = (me.sort.target == 0 || me.sort.target == 'all') ? '0' : '1'
 				let a = (me.sort.about == 0 || me.sort.about == 'all') ? '0' : '1'
 
 				let combination = '' + w + t + a
@@ -88,15 +88,15 @@
 						.then(() => me.$store.state.isLoading = false).catch(err => me.complaints = []).then(() => me.$store.state.isLoading = false)
 						break
 					case '110':
-						me.$binding('complaints', db.collection('complaints').where('way', '==', me.sort.way).where('status', '==', me.sort.status).orderBy('created_at', 'desc'))
+						me.$binding('complaints', db.collection('complaints').where('way', '==', me.sort.way).where('target', '==', me.sort.target).orderBy('created_at', 'desc'))
 						.then(() => me.$store.state.isLoading = false).catch(err => me.complaints = []).then(() => me.$store.state.isLoading = false)
 						break
 					case '111':
-						me.$binding('complaints', db.collection('complaints').where('way', '==', me.sort.way).where('status', '==', me.sort.target).where('about', '==', me.sort.about).orderBy('created_at', 'desc'))
+						me.$binding('complaints', db.collection('complaints').where('way', '==', me.sort.way).where('target', '==', me.sort.target).where('about', '==', me.sort.about).orderBy('created_at', 'desc'))
 						.then(() => me.$store.state.isLoading = false).catch(err => me.complaints = []).then(() => me.$store.state.isLoading = false)
 						break
 					case '011':
-						me.$binding('complaints', db.collection('complaints').where('status', '==', me.sort.status).where('about', '==', me.sort.about).orderBy('created_at', 'desc'))
+						me.$binding('complaints', db.collection('complaints').where('target', '==', me.sort.target).where('about', '==', me.sort.about).orderBy('created_at', 'desc'))
 						.then(() => me.$store.state.isLoading = false).catch(err => me.complaints = []).then(() => me.$store.state.isLoading = false)
 						break
 					case '001':
@@ -104,7 +104,7 @@
 						.then(() => me.$store.state.isLoading = false).catch(err => me.complaints = []).then(() => me.$store.state.isLoading = false)
 						break
 					case '010':
-						me.$binding('complaints', db.collection('complaints').where('status', '==', me.sort.status).orderBy('created_at', 'desc'))
+						me.$binding('complaints', db.collection('complaints').where('target', '==', me.sort.target).orderBy('created_at', 'desc'))
 						.then(() => me.$store.state.isLoading = false).catch(err => me.complaints = []).then(() => me.$store.state.isLoading = false)
 						break
 					case '111':
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,183 +3,54 @@
 		<div class="container" v-if="$store.state.isClientLoggedIn || $store.state.isAdminLoggedIn">
 			<form @submit.prevent="submitComplaint()">
 				<div class="form-group">
-					<label>Unit</label>
-					<input type="text" v-model="unit" name="unit" placeholder="none" v-validate="'required'">
-				</div>
-				<div class="form-group">
-					<label>Tower</label>
-					<select v-model="tower" v-validate="'excluded:none'">
+					<label>What/Who is the subject of your concern? *</label>
+					<select name="target" v-model="target">
 						<option value="none" style="display: none">Please select</option>
-						<option>North</option>
-						<option>South</option>
+						<option>Facilities</option>
+						<option>Instructor</option>
+						<option>Portal</option>
+						<option>Guidance</option>
+						<option>Staff</option>
+						<option>Event</option>
+						<option>Library</option>
+						<option>Registrar</option>
+					</select>
+				</div>
+				<div class="form-group" v-if="target == 'Instructor'">
+					<label>Department *</label>
+					<select name="target" v-model="department" v-validate="'excluded:none'">
+						<option value="none" style="display: none">Please select</option>
+						<option>College of Computing Sciences (CCS)</option>
+						<option>College of Technology (CT)</option>
+						<option>College of Education (CE)</option>
+						<option>College of Hospitality Management (CHM)</option>
+						<option>College of Business and Public Administration (CBPA)</option>
+						<option>Collge of Art Sciences and Letters (CASL)</option>
+					</select>
+				</div>
+				<div class="form-group" v-if="target == 'Staff'">
+					<label>Office *</label>
+					<select name="target" v-model="office" v-validate="'excluded:none'">
+						<option value="none" style="display: none">Please select</option>
+						<option>Cashier</option>
+						<option>Administrator</option>
+						<option>Registrar</option>
 					</select>
 				</div>
 				<div class="form-group">
-					<label>Month</label>
-					<select v-model="about">
+					<label>What is this all about? *</label>
+					<select v-model="about" v-validate="'excluded:none'">
 						<option value="none" style="display: none">Please select</option>
-						<option>January</option>
-						<option>February</option>
-						<option>March</option>
-						<option>April</option>
-						<option>May</option>
-						<option>June</option>
-						<option>July</option>
-						<option>August</option>
-						<option>September</option>
-						<option>October</option>
-						<option>November</option>
-						<option>December</option>
+						<option>Suggestion</option>
+						<option>Feedback</option>
 					</select>
 				</div>
-				<div class="form-group">
-					<label>Day</label>
-					<select v-model="target">
-						<option value="none" style="display: none">Please select</option>
-						<option>1</option>
-						<option>2</option>
-						<option>3</option>
-						<option>4</option>
-						<option>5</option>
-						<option>6</option>
-						<option>7</option>
-						<option>8</option>
-						<option>9</option>
-						<option>10</option>
-						<option>11</option>
-						<option>12</option>
-						<option>13</option>
-						<option>14</option>
-						<option>15</option>
-						<option>16</option>
-						<option>17</option>
-						<option>18</option>
-						<option>19</option>
-						<option>20</option>
-						<option>21</option>
-						<option>22</option>
-						<option>23</option>
-						<option>24</option>
-						<option>25</option>
-						<option>26</option>
-						<option>27</option>
-						<option>28</option>
-						<option>29</option>
-						<option>30</option>
-						<option>31</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label>Year</label>
-					<select v-model="way">
-						<option value="none" style="display: none">Please select</option>
-						<option>2018</option>
-						<option>2019</option>
-						<option>2020</option>
-						<option>2021</option>
-						<option>2022</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label>Time</label>
-					<select v-model="time">
-						<option value="none" style="display: none">Please select</option>
-						<option>AM</option>
-						<option>PM</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label>Hour</label>
-					<select v-model="hour">
-						<option value="none" style="display: none">Please select</option>
-						<option>1</option>
-						<option>2</option>
-						<option>3</option>
-						<option>4</option>
-						<option>5</option>
-						<option>6</option>
-						<option>7</option>
-						<option>8</option>
-						<option>9</option>
-						<option>10</option>
-						<option>11</option>
-						<option>12</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label>Minutes</label>
-					<select v-model="minutes">
-						<option value="none" style="display: none">Please select</option>
-						<option>00</option>
-						<option>1</option>
-						<option>2</option>
-						<option>3</option>
-						<option>4</option>
-						<option>5</option>
-						<option>6</option>
-						<option>7</option>
-						<option>8</option>
-						<option>9</option>
-						<option>10</option>
-						<option>11</option>
-						<option>12</option>
-						<option>13</option>
-						<option>14</option>
-						<option>15</option>
-						<option>16</option>
-						<option>17</option>
-						<option>18</option>
-						<option>19</option>
-						<option>20</option>
-						<option>21</option>
-						<option>22</option>
-						<option>23</option>
-						<option>24</option>
-						<option>25</option>
-						<option>26</option>
-						<option>27</option>
-						<option>28</option>
-						<option>29</option>
-						<option>30</option>
-						<option>31</option>
-						<option>32</option>
-						<option>33</option>
-						<option>34</option>
-						<option>35</option>
-						<option>36</option>
-						<option>37</option>
-						<option>38</option>
-						<option>39</option>
-						<option>40</option>
-						<option>41</option>
-						<option>42</option>
-						<option>43</option>
-						<option>44</option>
-						<option>45</option>
-						<option>46</option>
-						<option>47</option>
-						<option>48</option>
-						<option>49</option>
-						<option>50</option>
-						<option>51</option>
-						<option>52</option>
-						<option>53</option>
-						<option>54</option>
-						<option>55</option>
-						<option>56</option>
-						<option>57</option>
-						<option>58</option>
-						<option>59</option>
-					</select>
-				</div>
-
-
 				<div class="form-group">
 					<label>Your Complaint *</label>
 					<textarea rows="6" placeholder="Type your concern" v-model="message" name="message" v-validate="'required'"></textarea>
 				</div>
 				<div class="captcha-container">
-                    <vue-recaptcha class="g-recaptcha" sitekey="6Lc2nnYUAAAAAHlEIu1w9cM0iNPbwv2cj0dQS8rm"></vue-recaptcha>
+                    <vue-recaptcha class="g-recaptcha" sitekey="6Le-pnYUAAAAAGx0hkAt7zH_GX2xF3eGTIM66FOo"></vue-recaptcha>
                 </div>
 				<div class="buttons-container">
 					<button type="submit">Submit</button>
@@ -200,51 +71,6 @@
 					</div>
 					<div class="form-group">
 						<button type="submit">Login</button>
-						<div class="not">
-							Don't have an account? <a href="javascript:void()" @click="authState = 2">Click here to register</a>
-						</div>
-					</div>
-				</form>
-			</div>
-			<div class="container login-container" v-if="$store.state.isClientLoggedIn == false && authState == 2">
-				<img src="/logo.png" class="logo">
-				<p class="h3">Register</p>
-				<form @submit.prevent="clientRegister()">
-					<div class="form-group">
-						<input type="text" placeholder="Your ID Number (to be verified by admin)" v-model="reg.idnumber" name="reg_idnumber" autofocus v-validate="'required'">
-						<span class="validation-errors">{{ errors.first('reg_idnumber') }}</span>
-					</div>
-					<div class="form-group">
-						<input type="text" placeholder="Fullname" v-model="reg.fullname" name="reg_fullname" v-validate="'required|alpha_spaces'">
-						<span class="validation-errors">{{ errors.first('reg_fullname') }}</span>
-					</div>
-					<div class="form-group">
-						<input type="text" placeholder="Email" v-model="reg.email" name="reg_email" v-validate="'required|email'">
-						<span class="validation-errors">{{ errors.first('reg_email') }}</span>
-					</div>
-					<div class="form-group">
-						<select name="way" v-validate="'excluded:0'" v-model="reg.way">
-							<option value="0" disabled>What are you?</option>
-							<option>Student</option>
-							<option>Staff</option>
-							<option>Faculty</option>
-							<option>Graduate</option>
-						</select>
-						<span class="validation-errors">{{ (errors.first('way')) ? 'This field is required' : '' }}</span>
-					</div>
-					<div class="form-group">
-						<input type="password" placeholder="Password" v-model="reg.password" name="reg_password" v-validate="'required|min:8'" ref="reg_password">
-						<span class="validation-errors">{{ errors.first('reg_password') }}</span>
-					</div>
-					<div class="form-group">
-						<input type="password" placeholder="Confirm Password" v-model="reg.password2" name="reg_password2" v-validate="'required||confirmed:reg_password'">
-						<span class="validation-errors">{{ errors.first('reg_password2') }}</span>
-					</div>
-					<div class="form-group">
-						<button type="submit">Submit</button>
-						<div class="not">
-							Already have an account? <a href="javascript:void()" @click="authState = 1">Click here to login</a>
-						</div>
 					</div>
 				</form>
 			</div>
@@ -269,21 +95,15 @@
 		data () {
 			return{
 				// data to submit
-				message: '',
-				name: '',
-				alias: '',
-				unit: '',
-				tower: 'none',
 				about: 'none',
 				target: 'none',
-				way: 'none',
-				time: 'none',
-				hour: 'none',
-				minutes: 'none',
-
+				message: '',
+				ip: '',
+				name: '',
+				alias: '',
 
 				department: 'none',
-				position: 'none',
+				office: 'none',
 
 				// others
 				authState: 1,
@@ -293,17 +113,6 @@
 					username: '',
 					password: ''
 				},
-
-				// for registration
-				reg: {
-					idnumber: '',
-					username: '',
-					fullname: '',
-					email: '',
-					password: '',
-					password2: '',
-					way: 0
-				}
 			}
 		},
 		methods: {
@@ -334,7 +143,7 @@
 		                    me.$store.state.decodedClientToken = decoded
 			            })
 					}else{
-						alert('Invalid credentials or your account has not been approved by the administrator. Please try again.')
+						alert('Invalid credentials or Please try again.')
 						Cookie.remove('client-token')
 						me.$store.state.isClientLoggedIn = false
 					}
@@ -343,51 +152,6 @@
 				}).then(() => {
 					me.$store.state.isLoading = false
 				})
-			},
-			clientRegister() {
-				let me = this
-				// validation here
-				me.$validator.validate().then(res => {
-					if(!res) { // do not proceed
-						die()
-					}else{
-						// check if data exists
-						me.$store.state.isLoading = true
-						db.collection('users')
-						.where('idnumber', '==', me.reg.idnumber)
-						.get().then(res => {
-							if(!res.empty) { // if exists
-								alert('Sorry. It seems like the ID Number you provided is already registered')
-							}else{ // if doesnt exits
-								db.collection('users').doc().set({
-									idnumber: me.reg.idnumber,
-									fullname: me.reg.fullname,
-									email: me.reg.email,
-									password: me.reg.password,
-									role: 0,
-									approved: true,
-									created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
-									updated_at: moment().format('YYYY-MM-DD HH:mm:ss'),
-									way: me.reg.way
-								}).then(() => {
-									alert('Thank you for registering. The admin will approve your account within 24 hours.')
-									me.reg.idnumber = ''
-									me.reg.fullname = ''
-									me.reg.email = ''
-									me.reg.password = ''
-									me.reg.password2 = ''
-								}).catch(err => {
-									console.log('Error: '+err)
-								})
-							}
-						}).catch(err => { console.log('Error: '+error) })
-						.then(() => {
-							me.$store.state.isLoading = false
-						})
-
-					}
-				})
-				// end
 			},
 			submitComplaint() {
 				let me = this
@@ -421,21 +185,15 @@
 						let realName = me.$store.state.decodedClientToken.real_name
 						let idnumber = me.$store.state.decodedClientToken.idnumber
 						db.collection('complaints').doc().set({
-							alias: (me.alias == '') ? 'Anonymous' : me.alias,
-							way: me.way,
+							way: way,
 							about: me.about,
 							target: me.target,
-							unit: me.unit,
-							tower: me.tower,
-							time: me.time,
-							hour: me.hour,
-							minutes: me.minutes,
 							message: me.message,
 							user_id: userID,
 							showToPublic: true,
 							real_name: realName,
 							department: me.department,
-							position: me.position,
+							office: me.office,
 							created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
 							updated_at: moment().format('YYYY-MM-DD HH:mm:ss'),
 							status: 'Pending',
@@ -461,14 +219,8 @@
 				me.way = 'none'
 				me.about = 'none'
 				me.target = 'none'
-				me.unit = 'none'
-				me.tower = 'none'
-				me.time = 'none'
-				me.hour = 'none'
-				me.minutes = 'none'
 				me.message = ''
 				me.name = ''
-				me.alias = ''
 			},
 			setLoading(val) {
 				this.$store.state.isLoading = val
